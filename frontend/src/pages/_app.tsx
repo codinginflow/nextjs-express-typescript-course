f7d54ebe24d1ed7be9ff49db6921508a5ed2e5d4
Coding in Flow <info@codinginflow.com>
2023-03-24 05:33:47 -0700

    End of part 3

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Flow Blog - Share your ideas</title>
+        <meta name="description" content="A full-stack NextJS course project by Coding in Flow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={inter.className}>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
 }
---
+++ frontend/src/pages/blog.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function BlogPage() {
+    return (
+        <>
+            <Head>
+                <title>Articles - Flow Blog</title>
+                <meta name="description" content="Read the latest posts on Flow Blog" />
+            </Head>
+
+            <div>
+                Hello, blog!
+            </div>
+        </>
+    );
+}
